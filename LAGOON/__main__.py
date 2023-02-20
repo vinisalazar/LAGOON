d134af59c1202614958b72c59ddfc8c3bfd35694
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-02-20 15:59:59 +1100

    Add dunder modules

---
+++ LAGOON/__main__.py
@@ -0,0 +1,160 @@
+"""
+Entrypoint for LAGOON
+
+Check out the wiki for a detailed look at customising this file:
+https://github.com/beardymcjohnface/Snaketool/wiki/Customising-your-Snaketool
+"""
+
+import os
+import click
+
+from .util import (
+    snake_base,
+    get_version,
+    default_to_output,
+    copy_config,
+    run_snakemake,
+    OrderedCommands,
+    print_citation,
+)
+
+
+def common_options(func):
+    """Common command line args
+    Define common command line args here, and include them with the @common_options decorator below.
+    """
+    options = [
+        click.option(
+            "--output",
+            help="Output directory",
+            type=click.Path(dir_okay=True, writable=True, readable=True),
+            default="lagoon_wf.out",
+            show_default=True,
+        ),
+        click.option(
+            "--configfile",
+            default="config.yaml",
+            show_default=False,
+            callback=default_to_output,
+            help="Custom config file [default: (outputDir)/config.yaml]",
+        ),
+        click.option(
+            "--threads", help="Number of threads to use", default=1, show_default=True
+        ),
+        click.option(
+            "--use-conda/--no-use-conda",
+            default=True,
+            help="Use conda for Snakemake rules",
+            show_default=True,
+        ),
+        click.option(
+            "--conda-prefix",
+            default=snake_base(os.path.join("workflow", "conda")),
+            help="Custom conda env directory",
+            type=click.Path(),
+            show_default=False,
+        ),
+        click.option(
+            "--snake-default",
+            multiple=True,
+            default=[
+                "--rerun-incomplete",
+                "--printshellcmds",
+                "--nolock",
+                "--show-failed-logs",
+            ],
+            help="Customise Snakemake runtime args",
+            show_default=True,
+        ),
+        click.option(
+            "--log",
+            default="lagoon_wf.log",
+            callback=default_to_output,
+            hidden=True,
+        ),
+        click.argument("snake_args", nargs=-1),
+    ]
+    for option in reversed(options):
+        func = option(func)
+    return func
+
+
+@click.group(
+    cls=OrderedCommands, context_settings=dict(help_option_names=["-h", "--help"])
+)
+@click.version_option(get_version(), "-v", "--version", is_flag=True)
+def cli():
+    """LArGe cOmparative Omics Networks (LAGOON) is designed to treat large omics data. It has been made by the unit of environmental genomics of the Muséum National d'Histoire Naturelle based in Paris, FRANCE.
+    \b
+    For more options, run:
+    lagoon_wf command --help"""
+    pass
+
+
+help_msg_extra = """
+\b
+CLUSTER EXECUTION:
+lagoon_wf run ... --profile [profile]
+For information on Snakemake profiles see:
+https://snakemake.readthedocs.io/en/stable/executing/cli.html#profiles
+\b
+RUN EXAMPLES:
+Required:           lagoon_wf run --input [file]
+Specify threads:    lagoon_wf run ... --threads [threads]
+Disable conda:      lagoon_wf run ... --no-use-conda 
+Change defaults:    lagoon_wf run ... --snake-default="-k --nolock"
+Add Snakemake args: lagoon_wf run ... --dry-run --keep-going --touch
+Specify targets:    lagoon_wf run ... all print_targets
+Available targets:
+    all             Run everything (default)
+    print_targets   List available targets
+"""
+
+
+@click.command(
+    epilog=help_msg_extra,
+    context_settings=dict(
+        help_option_names=["-h", "--help"], ignore_unknown_options=True
+    ),
+)
+@click.option("--input", "_input", help="Input file/directory", type=str, required=True)
+@common_options
+def run(_input, output, log, **kwargs):
+    """Run LAGOON"""
+    # Config to add or update in configfile
+    merge_config = {"input": _input, "output": output, "log": log}
+
+    # run!
+    run_snakemake(
+        # Full path to Snakefile
+        snakefile_path=snake_base(os.path.join("workflow", "Snakefile")),
+        merge_config=merge_config,
+        log=log,
+        **kwargs
+    )
+
+
+@click.command()
+@common_options
+def config(configfile, **kwargs):
+    """Copy the system default config file"""
+    copy_config(configfile)
+
+
+@click.command()
+def citation(**kwargs):
+    """Print the citation(s) for this tool"""
+    print_citation()
+
+
+cli.add_command(run)
+cli.add_command(config)
+cli.add_command(citation)
+
+
+def main():
+    cli()
+
+
+if __name__ == "__main__":
+    main()
